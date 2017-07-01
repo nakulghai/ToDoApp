5661192173e5736066374eac91bab759d2a216c1
Nakul Ghai <Nakul Ghai>
2017-07-01 20:25:13 +0530

    Starting Project

--- Components/MainPage.jsx
+++ Components/MainPage.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import NavBar from './NavBar.jsx';
-var redux = require('redux');
-import axios from 'axios';
-var actions = require ('./../actions/index.jsx');
 
-var store = require ('./../stores/configureStore.jsx').configure();;
 
 
 class MainPage extends React.Component {
@@ -22,28 +18,9 @@ class MainPage extends React.Component {
   }
 
   render () {
-
-    store.subscribe(() => {
-      let val = store.getState();
-      console.log('currentstate',val);
-      let st = store.getState();
-      if(!st.map.isFetching) {
-        console.log("inner html");
-        document.getElementById("app").innerHTML = '<a href="'+st.map.url+'" target="_blank">click</a>'
-      }
-    });
-    store.dispatch(actions.mapFetching());
-    store.dispatch(actions.changeName("nakul"));
-    store.dispatch(actions.changeName("rahul"));
-    store.dispatch(actions.changeMovie('BBB'));
-    store.dispatch(actions.removeMovie('BBB'));
     return(
-      <div id="app1">
-        MainPage {this.state.name}
-        {1+1}{store.getState()}
-        <NavBar />
-        <input type='text' onChange={this.handleChange} />
-        <p>value of input is {this.state.value}</p>
+      <div>
+      MainPag
       </div>
     );
   }
